5217e2ccc671aff71ee759e50254a2faa9ccd807
Florent Uguet <florent.uguet@gmail.com>
2015-03-26 19:52:18 -0400

    Added headers.h

---
+++ sources/BooruHeaders/headers.h
@@ -0,0 +1,20 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include "../BooruHeaders/generic_booru_image.h"
+#include "../BooruHeaders/generic_booru_fonctions.h"
+
+#include "../BooruHeaders/derpibooru_recherche.h"
+#include "../BooruHeaders/derpibooru_fonctions.h"
+
+#include "../BooruHeaders/gelbooru_recherche.h"
+#include "../BooruHeaders/gelbooru_fonctions.h"
+
+#include "../BooruHeaders/moebooru_recherche.h"
+#include "../BooruHeaders/moebooru_fonctions.h"
+
+#include "../BooruHeaders/danbooru_recherche.h"
+#include "../BooruHeaders/danbooru_fonctions.h"
+
+#endif // HEADERS_H
+
