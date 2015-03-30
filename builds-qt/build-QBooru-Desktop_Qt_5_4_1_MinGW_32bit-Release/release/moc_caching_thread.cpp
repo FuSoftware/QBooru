e88e08d9ed59ac42ddae6d2355289c5fafe6273f
Florent Uguet <florent.uguet@gmail.com>
2015-03-30 19:45:56 -0400

    Build 1.7.5.1

--- builds-qt/build-QBooru-Desktop_Qt_5_4_1_MinGW_32bit-Release/release/moc_caching_thread.cpp
+++ builds-qt/build-QBooru-Desktop_Qt_5_4_1_MinGW_32bit-Release/release/moc_caching_thread.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../sources/viewer/Workers/caching_thread.h"
+#include "../../../src/viewer/Workers/caching_thread.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- builds-qt/build-QBooru-Desktop_Qt_5_4_1_MinGW_32bit-Release/release/moc_imageTabClass.cpp
+++ builds-qt/build-QBooru-Desktop_Qt_5_4_1_MinGW_32bit-Release/release/moc_imageTabClass.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../sources/viewer/Widget/imageTabClass.h"
+#include "../../../src/viewer/Widget/imageTabClass.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
