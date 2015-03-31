a48299edc243be068b0a536a55c478b85d8d6c5f
Florent Uguet <florent.uguet@gmail.com>
2015-03-30 21:15:38 -0400

    New build

--- src/viewer/constantes.h
+++ src/viewer/constantes.h
@@ -27,14 +27,14 @@
 
 #define CURL_STATICLIB
 #define APP_NAME "QBooru Viewer"
-#define APP_VERSION "1.7.5.1"
+#define APP_VERSION "1.7.5.2"
 #define TAG_NUMBER    512 //Max number of tags to show in the Viewer
 #define MAX_BOORU_NUMBER 64 //Max number of Boorus to load in the main widget
 #define MAX_PICTURE_NUMBER 64 //Max number of pictures/page
 
-#define CONF_FILE_URL "http://www.aht.li/2475858/configuration_-_defaut.json"
-#define CHANGELOG_FILE_URL "http://www.aht.li/2460180/changelog.txt"
-#define LAST_VERSION_FILE_URL "http://www.aht.li/2475899/lastVersion.json"
+#define CONF_FILE_URL "https://raw.githubusercontent.com/FlorentUguet/QBooru/master/builds/latest/conf/configuration.json"
+#define CHANGELOG_FILE_URL "https://github.com/FlorentUguet/QBooru/raw/master/changelog.html"
+#define LAST_VERSION_FILE_URL "https://github.com/FlorentUguet/QBooru/raw/master/versions.json"
 
 enum InfoLevel
 {
