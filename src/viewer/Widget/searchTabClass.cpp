0394d188c00207c176962ab6cae82aa6f32bada1
FlorentUguet <florent.uguet@gmail.com>
2015-10-24 13:20:14 +0200

    Debug to know longest word

--- src/viewer/Widget/searchTabClass.cpp
+++ src/viewer/Widget/searchTabClass.cpp
@@ -55,6 +55,7 @@ SearchTab::SearchTab(Widget *parent, BooruSite* site) : QWidget(parent)
                 if(booru->isTagListLoaded())
                 {
                     outputInfo(L_DEBUG,"Loading taglist for " + booru->getName());
+                    outputInfo(L_DEBUG,"Longest word is " + booru->getTagList()->getTag(booru->getTagList()->getMaxTagLengthIndex())->getName() + " with " + intToString(booru->getTagList()->getMaxTagLength()) +  " char");
                     QStringList wordList;
                     for(int i=0;i<booru->getTagList()->size();i++)
                     {
