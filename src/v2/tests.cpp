2a13fda8902ace6dad0c5b383518f13759ee9876
FlorentUguet <florent.uguet@gmail.com>
2016-06-13 19:02:13 +0200

    Added tests

---
+++ src/v2/tests.cpp
@@ -0,0 +1,34 @@
+#include "tests.h"
+
+void test_search()
+{
+    BooruSite* site = new BooruSite(string("Safebooru"),string("http://safebooru.org"), API::GELBOORU);
+
+    cout << "Site " << site->getName() << " has search url " << site->getSearchUrl() << endl;
+
+    BooruSearchEngine *engine = new BooruSearchEngine(site);
+
+    engine->search("moriya_suwako",0,15);
+}
+
+void test_site()
+{
+    BooruSite* site = new BooruSite(string("Safebooru"),string("http://safebooru.org"), API::GELBOORU);
+
+    cout << "Site " << site->getName() << " has search url " << site->getSearchUrl() << endl;
+}
+
+void test_database()
+{
+    BoorusDatabase *db = new BoorusDatabase();
+
+    BooruSite* saf = new BooruSite(string("Safebooru"),string("http://safebooru.org"), API::GELBOORU);
+    BooruSite* kon = new BooruSite(string("Konachan"),string("http://konachan.net"), API::MOEBOORU);
+
+    db->addBooru(saf);
+    db->addBooru(kon);
+
+    std::vector<BooruSite*> sites = db->loadBoorus();
+
+    cout << sites.size() << " boorus loaded" << endl;
+}
---
+++ src/v2/tests.h
@@ -0,0 +1,17 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <iostream>
+#include <vector>
+
+#include "model/boorusite.h"
+#include "controller/boorusearchengine.h"
+#include "controller/boorusdatabase.h"
+
+using namespace std;
+
+void test_search();
+void test_site();
+void test_database();
+
+#endif // TESTS_H
